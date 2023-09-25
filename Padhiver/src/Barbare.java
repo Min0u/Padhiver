afcb59bfb2c5d6581e57ff371dfd74365551115e
_Mi.n0u_ <105317987+Min0u@users.noreply.github.com>
2023-09-25 23:34:36 +0200

    Delete Padhiver directory

--- Padhiver/src/Barbare.java
+++
@@ -1,7 +0,0 @@
-public class Barbare extends Metier {
-    public Barbare() {
-        super("Barbare", "Hache", 5, 9, 0);
-    }
-
-    // Ajoutez des méthodes spécifiques au Barbare si nécessaire
-}
--- Padhiver/src/Orc.java
+++
@@ -1,7 +0,0 @@
-public class Orc extends Monstre {
-    public Orc() {
-        super("Orc", 8, 5, 0);
-    }
-
-    // Ajoutez ici des méthodes spécifiques à l'Orc si nécessaire
-}
